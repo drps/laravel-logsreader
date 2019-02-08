b9e6d5af3a2808fdd1c0b115218d49cf61fc6e3b
drps <akademball@gmail.com>
2019-02-08 16:29:20 +0700

    new seeder algorythm

--- database/factories/LogsFactory.php
+++ database/factories/LogsFactory.php
@@ -1,17 +1,15 @@
 <?php
 
 use App\Logs;
-use Carbon\Carbon;
 use Faker\Generator as Faker;
 
-$logs = DB::select('select max(dt) maxdt FROM logs');
-$startDatetime = Carbon::createFromFormat('Y-m-d H:i:s', $logs[0]->maxdt ?: date('Y-m-d H:i:s'));
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(Logs::class, function (Faker $faker) use ($startDatetime) {
+$factory->define(Logs::class, function (Faker $faker, $startDatetime) {
+    $from = $startDatetime[0];
     return [
         'message' => $faker->sentence(3),
         'type' => $faker->numberBetween(1,10),
-        'dt' => $startDatetime->addSeconds(TimeGenerator::getInstance()->add(random_int(1, 4))),
+        'dt' => $from,
     ];
 });
--- database/seeds/LogsSeeder.php
+++ database/seeds/LogsSeeder.php
@@ -1,23 +1,29 @@
 <?php
 
 use App\Logs;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class LogsSeeder extends Seeder
 {
     public function run()
     {
+        $logs = DB::select('select max(dt) maxdt FROM logs');
+        $startDatetime = Carbon::createFromFormat('Y-m-d H:i:s', $logs[0]->maxdt ?: date('Y-m-d H:i:s'));
+
+        $seconds = 0;
         $count = 0;
         for ($j = 0; $j < 10; $j++) {
             for ($i = 0; $i < 10; $i++) {
-                $logs = factory(Logs::class, 20000)->make()->all();
+                $logs = factory(Logs::class, 20000)->make([$startDatetime])->all();
                 $chunksize = 100;
 
                 foreach (array_chunk($logs, $chunksize) as $chunk) {
                     $data = array();
                     foreach ($chunk as $log) {
+                        $seconds += random_int(1,4);
                         $data[] = [
-                            'dt' => $log->dt,
+                            'dt' => $log->dt->addSeconds($seconds),
                             'message' => $log->message,
                             'type' => $log->type,
                         ];
@@ -25,7 +31,7 @@ class LogsSeeder extends Seeder
                     Logs::insert($data);
                     $count += $chunksize;
                 }
-                echo 'inserted ' . $count . ' rows ' . memory_get_peak_usage(true) . PHP_EOL;
+                echo 'inserted ' . $count . ' rows; ' . 'Last date ' . $data[count($data) - 1]['dt']->format('Y-m-d H:i:s') . '; Memory peak:' . memory_get_peak_usage(true) . PHP_EOL;
             }
         }
     }
