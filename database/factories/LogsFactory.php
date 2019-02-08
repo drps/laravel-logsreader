a0ddc8a172ed99f350a40d7b74a7f40d3fd8f36a
drps <akademball@gmail.com>
2019-02-08 15:36:23 +0700

    LogsFactory bugfix for duplicated timestamp

--- database/factories/LogsFactory.php
+++ database/factories/LogsFactory.php
@@ -4,11 +4,14 @@ use App\Logs;
 use Carbon\Carbon;
 use Faker\Generator as Faker;
 
+$logs = DB::select('select max(dt) maxdt FROM logs');
+$startDatetime = Carbon::createFromFormat('Y-m-d H:i:s', $logs[0]->maxdt ?: date('Y-m-d H:i:s'));
+
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(Logs::class, function (Faker $faker) {
+$factory->define(Logs::class, function (Faker $faker) use ($startDatetime) {
     return [
         'message' => $faker->sentence(3),
         'type' => $faker->numberBetween(1,10),
-        'dt' => Carbon::now()->addSeconds(TimeGenerator::getInstance()->add(random_int(1, 20))),
+        'dt' => $startDatetime->addSeconds(TimeGenerator::getInstance()->add(random_int(1, 4))),
     ];
 });
